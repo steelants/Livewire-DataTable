5250df13b2206290f5aca9bad07c8fd6e6a1a55a
JonatanRek <gamerclassn7@gmail.com>
2024-02-06 11:50:35 +0100

    Merge branch

---
+++ resources/views/livewire/data-table-v2.blade.php
@@ -0,0 +1,96 @@
+<div>
+    @if ($dataset != null)
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        @foreach ($headers as $header)
+                            {{-- Nespoléhat se na proměnou headers může být uplně jiná než property sortovat přes funkci --}}
+                            <th @if($sortable) @if ($header != $sortBy) wire:click="$set('sortBy','{{ $header }}')" @else wire:click="$set('sortDesc','{{ !$sortDesc }}')" @endif  @endif scope="col">
+                                @if($sortable)
+                                    @if ($header != $sortBy)
+                                        ↕
+                                    @else
+                                        @if ($sortDesc)
+                                            ↑
+                                        @else
+                                            ↓
+                                        @endif
+                                    @endif
+                                @endif
+                                {{ ucwords($header) }}
+                            </th>
+                        @endforeach
+                        @if (method_exists($this, 'actions'))
+                            <th>
+                                {{ __('datatable::ui.actions') }}
+                            </th>
+                        @endif
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($dataset as $row)
+                        <tr>
+                            @foreach ($row as $key => $collum)
+                                @if($key != "id" || ($key == "id" && in_array("id", $headers)))
+                                    <td>{{ $collum }}</td>
+                                @endif
+                            @endforeach
+                            @if (method_exists($this, 'actions'))
+                                <td>
+                                    @foreach ($this->actions($row) as $action)
+                                        @if ($action['type'] == 'route')
+                                            <a href="{{ route($action['name'], $action['parameters']) }}"> {{ __($action['name']) }}</a>
+                                        @elseif ($action['type'] == 'livewire')
+                                            <button wire:click='{{ $action['action'] }}({{ $action['parameters'] }})'> {{ __($action['name']) }}</button>
+                                        @else
+                                            {{ __('datatable::ui.actions.not_implemented') }}
+                                        @endif
+                                    @endforeach
+                                </td>
+                            @endif
+                        </tr>
+                    @endforeach
+                </tbody>
+                @if (!empty($footers))
+                    <tfoot>
+                        @foreach ($footers as $footer)
+                            <th scope="col">{{ $footer }}</th>
+                        @endforeach
+                    </tfoot>
+                @endif
+            </table>
+        </div>
+        @if ($paginated == true)
+            <div class="d-flex justify-content-between">
+                <nav aria-label="Page navigation example">
+                    @if ($pagesTotal > 1)
+                        <ul class="pagination">
+                            @php($pagesIndex = max(0, min($pagesIndex, $pagesTotal)))
+                            @php($startPage = max(0, $pagesIndex - intval(7 / 2)))
+                            @php($endPage = min($pagesTotal, $startPage + 7 - 1))
+                            @if ($pagesIndex > 0)
+                                <li class="page-item"><a class="page-link" wire:click.prevent="$set('pagesIndex', {{ $pagesIndex - 1 }})">Previous</a></li>
+                            @endif
+                            @for ($i = $startPage; $i < $endPage; $i++)
+                                <li class="page-item"><a @if ($i != $pagesIndex) wire:click.prevent="$set('pagesIndex', {{ $i }})" @endif class="page-link @if ($i == $pagesIndex) active @endif">{{ $i }}</a></li>
+                            @endfor
+                            @if ($pagesIndex < ($pagesTotal -1 ))
+                                <li class="page-item"><a class="page-link" wire:click.prevent="$set('pagesIndex', {{ $pagesIndex + 1 }})">Next</a></li>
+                            @endif
+                        </ul>
+                    @endif
+                </nav>
+                <div>
+                    <select class="form-select" wire:model="itemsPerPage">
+                        @foreach ([10, 20, 50, 100, 1000] as $itemsPerPage)
+                            <option value="{{ $itemsPerPage }}">{{ $itemsPerPage != 0 ? $itemsPerPage : 'custom' }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+        @endif
+    @else
+        <p>{{ __('datatable::ui.nothing_found') }}</p>
+    @endif
+</div>
---
+++ src/Http/Livewire/DataTableV2.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace SteelAnts\DataTable\Http\Livewire;
+
+use Exception;
+use Livewire\Component;
+use Illuminate\Database\Eloquent\Builder;
+
+class DataTableV2 extends Component
+{
+    /* RUNTIME VARIABLES */
+    protected $dataset = [];
+
+    public int $pagesTotal = 1;
+    public int $pagesIndex = 0;
+
+    /* SORTING VARIABLES */
+    public $sortable = false;
+    public $sortBy;
+    public bool $sortDesc = true;
+
+    /* PAGINATION */
+    public $paginated = false;
+    public int $itemsPerPage = 0;
+
+    // public function query(): Builder
+    // {
+    //      return Model::where('id','>',0)->limit(100);
+    // }
+
+    public function dataset(): array
+    {
+        return [
+            [
+                "cool1" => "test1",
+                "cool2" => "test2",
+            ],
+        ];
+    }
+
+    // public function row($row) : array
+    // {
+    //     return $row;
+    // }
+
+    // public function colum($colum)
+    // {
+    //     return "value";
+    // }
+
+    public function headers(): array
+    {
+        return array_keys($this->dataset[0]);
+    }
+
+    public function footers(): array
+    {
+        return [];
+        // $footer = [];
+        // $footer[] = "Count";
+        // for ($item=1; $item < count($this->dataset[0]); $item++) {
+        //     $footer[] = "";
+        // }
+        // $footer[] = count($this->dataset);
+        // return $footer;
+    }
+
+    public function updatedItemsPerPage()
+    {
+        $this->pagesIndex = 0;
+    }
+
+    public function updatedPageIndex()
+    {
+        $this->getData(true);
+    }
+
+    public function queryString(): array
+    {
+        $queryStrings = [];
+        if ($this->paginated == true) {
+            $queryStrings[] = 'pagesIndex';
+        }
+        if ($this->itemsPerPage != 0) {
+            $queryStrings[] = 'itemsPerPage';
+        }
+        if ($this->sortable != false) {
+            $queryStrings[] = 'sortBy';
+            if (!empty($this->sortBy)) {
+                $queryStrings[] = 'sortDesc';
+            }
+        }
+        return $queryStrings;
+    }
+
+    public function render()
+    {
+        return view('datatable::data-table-v2', [
+            'dataset' => $this->getData(),
+            'headers' => $this->getHeader(),
+            'footers' => $this->footers(),
+        ]);
+    }
+
+    private function getData($force = false): array
+    {
+        $itemsTotal = 0;
+        if ($this->dataset != [] && $force != true) {
+        } else if (method_exists($this, "query")) {
+            $datasetFromDB = [];
+            $query = $this->query();
+            $itemsTotal = $query->count();
+
+            if ($this->paginated != false) {
+                $query->limit($this->itemsPerPage);
+                if ($this->pagesIndex > 0) {
+                    $query = $query->offset($this->itemsPerPage * $this->pagesIndex);
+                }
+            }
+
+            foreach ($query->get() as $item) {
+                $tempRow = (method_exists($this, "row") ? $this->{"row"}($item) : $item->toArray());
+                $tempRow['id'] = $item->id;
+                foreach ($tempRow as $key => $property) {
+                    $tempRow[$key] = (method_exists($this, "colum{$key}Data") ? $this->{"collum{$key}Data"}($property) : $property);
+                }
+                $datasetFromDB[] = $tempRow;
+            }
+            $this->dataset = $datasetFromDB;
+        } else {
+            $this->dataset = $this->dataset();
+            $itemsTotal = $this->dataset->count();
+        }
+
+        if ($this->paginated != false && $this->itemsPerPage != 0) {
+            $this->pagesTotal = round(ceil($itemsTotal / $this->itemsPerPage));
+        }
+
+        $finalCollection = collect($this->dataset);
+        if ($this->sortable) {
+            $finalCollection = $finalCollection->sortBy($this->sortBy, SORT_REGULAR, $this->sortDesc);
+        }
+
+        return $finalCollection->toArray();
+    }
+
+
+    private function getHeader(): array
+    {
+
+        if (!method_exists($this, "headers")) {
+            return [];
+        }
+
+        if ($this->dataset == []) {
+            return $this->headers();
+        }
+
+        $datasetHeadersCount = count(array_keys($this->dataset[0]));
+        if ($datasetHeadersCount != count($this->headers())) {
+            if (($datasetHeadersCount - 1) != count($this->headers())) {
+                throw new Exception("Number of porperties (" . count(array_keys($this->dataset[0])) . "), need to be equal to number of headers (" . count($this->headers()) . ")");
+            }
+        }
+
+        return $this->headers();
+    }
+
+    // public function actions($item): array
+    // {
+    //     return [
+    //         [
+    //             'type' => "route",
+    //             'class' => "danger",
+    //             'name' => 'task.show',
+    //             'parameters' => [
+    //                 'task' => $item['id'],
+    //             ],
+    //         ],
+    //         [
+    //             'type' => "livewire",
+    //             'action' => 'showModal',
+    //             'parameters' => [
+    //                 'task' => $item['id'],
+    //             ],
+    //         ],
+    //     ];
+    // }
+}
