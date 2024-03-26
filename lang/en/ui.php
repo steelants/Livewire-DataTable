1adcdedff9ddd037849b2c0b33df3a13a9773b7e
Patrik Sztefek <patrik.sztefek@gmail.com>
2024-03-26 17:17:13 +0100

    empty state

--- lang/en/ui.php
+++ lang/en/ui.php
@@ -3,5 +3,5 @@
 return [
     "actions"=>"Actions",
     "actions.not_implemented"=>"Actions not implemented!",
-    "nothing_found" => "Nothing found :("
+    "nothing_found" => "Nothing found"
 ];
